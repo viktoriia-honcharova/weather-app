1e67edc3fd193e73f3ee2981161488257fb60e55
Viktoriia Honcharova <vika.honcharova.619853@gmail.com>
2022-07-21 12:58:45 +0300

    Overwriting the js file

--- src/index.js
+++ src/index.js
@@ -31,21 +31,7 @@ let dateTime = document.querySelector(".day-time");
 dateTime.innerHTML = formatDate(now);
 
 //Show data
-function searchCity(city) {
-  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}&units=metric`;
-  let apiKey = "7b520c6d96772777c0b1b903686b2ce1";
-  axios.get(`${apiUrl}&appid=${apiKey}`).then(displayWeather);
-}
-
-function changeData(event) {
-  event.preventDefault();
-  let cityInput = document.querySelector("#input");
-  let city = cityInput.value;
-
-  searchCity(city);
-}
-
-function displayWeather(responce) {
+/*function displayWeather(responce) {
   let cityName = document.querySelector("#city-name");
   let cityNameValue = responce.data.name;
   cityName.innerHTML = cityNameValue;
@@ -61,6 +47,20 @@ function displayWeather(responce) {
   windSpeed.innerHTML = Math.round(responce.data.wind.speed);
 
   console.log(responce);
+}
+
+function searchCity(city) {
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=${city}`;
+  let apiKey = "7b520c6d96772777c0b1b903686b2ce1";
+  axios.get(`${apiUrl}&appid=${apiKey}&units=metric`).then(displayWeather);
+}
+
+function changeData(event) {
+  event.preventDefault();
+  let cityInput = document.querySelector("#input");
+  let city = cityInput.value;
+
+  searchCity(city);
 }
 
 let cityForm = document.querySelector("#city-form");
@@ -83,4 +83,22 @@ function getCurrentPosition() {
 let locationButton = document.querySelector("#current-location");
 locationButton.addEventListener("click", getCurrentPosition);
 
-searchCity("Kyiv");
+searchCity("Kyiv");*/
+
+function displayTemperature(responce) {
+  console.log(responce.data);
+  let temp = Math.round(responce.data.main.temp);
+  document.querySelector("#current-temp").innerHTML = temp;
+  let city = responce.data.name;
+  document.querySelector("#city-name").innerHTML = city;
+  let humidity = responce.data.main.humidity;
+  document.querySelector("#humidity").innerHTML = humidity;
+  let wind = Math.round(responce.data.wind.speed);
+  document.querySelector("#wind-speed").innerHTML = wind;
+}
+
+let apiKey = "7b520c6d96772777c0b1b903686b2ce1";
+let apiUrl = `https://api.openweathermap.org/data/2.5/weather?q=Rivne&appid=${apiKey}&units=metric`;
+
+console.log(apiUrl);
+axios.get(apiUrl).then(displayTemperature);
